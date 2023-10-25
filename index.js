fd666001a2bcfca4a1c5c7a7ec443c41f7493742
programmer-er <jhariharan2004@gmail.com>
2023-10-25 21:52:04 +0530

    Added basic API with ExpressJS

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import express from 'express'
+
 import fs from 'fs/promises'
 import Robot from './Robot.js'
 
-let robot = new Robot()
-let svg = robot.draw()
 
-fs.writeFile("./output.svg", svg)
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	let robot = new Robot()
+	let svg = robot.draw()
+
+    res.set("Content-Type", "image/svg+xml")
+    res.send(Buffer.from(svg))
+})
+
+app.listen(port, () => {
+    console.log("App listening!")
+  })
